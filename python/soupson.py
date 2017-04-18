1bc1dd1abe28624b8bf1a7e4e169f01757fb1ab2
streetturtle <streetturtle@users.noreply.github.com>
2017-04-18 11:43:05 -0400

    Fix UnicodeEncodeError

--- python/soupson.py
+++ python/soupson.py
@@ -8,7 +8,7 @@ soup = BeautifulSoup(requests.get('http://www.soupson.ca/?lang=en').text, "lxml"
 
 menu = ""
 for row in soup.find_all("div", class_="entry-content")[0].find_all("p")[1:]:
-    menu += row.string + "\n"
+    menu += row.string.encode('ascii', 'ignore') + "\n"
 
 if len(sys.argv) > 1:
     f1=open('./' + sys.argv[1], 'w+')
